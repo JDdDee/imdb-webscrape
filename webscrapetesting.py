a2b4861e946ef0e8b8e3a62f8e5b20cd846dd089
JDdDee <jdchen333@gmail.com>
2022-04-05 02:34:58 -0400

    added in getmovielinks function, the program can grab arbitrary number of links to movie pages now

--- webscrapetesting.py
+++ webscrapetesting.py
@@ -17,10 +17,52 @@ from bs4 import BeautifulSoup
 import requests
 
 
+
 # get the html data from the imdb comedy movie list
 imdb_url = 'https://www.imdb.com'
-html_text = requests.get('https://www.imdb.com/search/title/?genres=comedy&explore=title_type,genres&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=3396781f-d87f-4fac-8694-c56ce6f490fe&pf_rd_r=9ARWMHY5YKJ7EFVTNE0V&pf_rd_s=center-1&pf_rd_t=15051&pf_rd_i=genre&ref_=ft_gnr_pr1_i_1').text
+html_text = requests.get('https://www.imdb.com/search/title/?genres=comedy&start=1&explore=title_type,genres&ref_=adv_nxt').text
 soup = BeautifulSoup(html_text, 'lxml')
+
+# given an abitrary page in the listing of movies, get the next page
+def getNextPageURL(url):
+    soup = BeautifulSoup(requests.get(url).text,'lxml')
+    nextlink = imdb_url + soup.find('a', class_ ='lister-page-next next-page').get('href')
+    return str(nextlink)
+"end def"
+
+# get movie links
+def getMovieLinks():
+    # this url is the one for comedy movies, if you want 1000 movies of a different genre, just swap this url
+    url = 'https://www.imdb.com/search/title/?genres=comedy&start=1&explore=title_type,genres&ref_=adv_nxt'
+    
+    movieLinkList = []
+    for i in range(0,10): # this range number can be modified to get a designated amt of links
+        soup = BeautifulSoup(requests.get(url).text, 'lxml')
+        moviecards = soup.find_all('h3', class_ = 'lister-item-header')
+        for m in moviecards:
+            movieLinkList.append(imdb_url + m.a.get('href'))
+        url = getNextPageURL(url)
+    "end for"
+
+    return movieLinkList
+
+"end def"
+
+# given the link to a moviepage, get all the info that we may want.
+# link to the google doc with schemas : https://docs.google.com/document/d/1u5GSzlVB__nNwAN6gUZsgE6nMR-T4LVavB8agKqINrY/edit
+# will likely be very very long
+def getMovieInfo(moviePageUrl):
+    soup = BeautifulSoup(requests.get(moviePageUrl).text, 'lxml')
+    
+    temp = []
+    # budget: there is no budget listing for movies
+    title = soup.find('h1', class_ = 'sc-b73cd867-0 eKrKux').text #Name
+    grossingWorldwide = soup.find('li', class_ = "ipc-metadata-list__item sc-3c7ce701-2 eYXppQ").find('span').find_next_sibling().text # grossing worldwide
+    # had to use attrs={key : value} for this because the key had a hyphen in it and python doesnt like that
+    releaseDate = soup.find('li', attrs={'data-testid' : 'title-details-releasedate'}).a.find_next_sibling().text
+
+    return temp
+"end def"
 
 #movies = soup.find_all('div', class_ = 'lister-list')
 
@@ -45,10 +87,10 @@ title = movie_soup.find('h1', class_ = 'TitleHeader__TitleText-sc-1wu6n3d-0 dxSW
 importantpeople = movie_soup.find('ul', class_ = 'ipc-metadata-list ipc-metadata-list--dividers-all title-pc-list ipc-metadata-list--baseAlt')
 directors = importantpeople.find_all('li', class_ = 'ipc-metadata-list__item')
 
+# prints all the important people that contributed to the movie
 for test in directors:
     print(test.text)
 
-# prints all the attributes that title has
-# print(title.attrs)
+
 
 
