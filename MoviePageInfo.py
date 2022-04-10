9d7ff37692c93d06fb5df5fa172fa8e9481f644c
Jared Chen <94023293+JDdDee@users.noreply.github.com>
2022-04-10 19:12:45 -0400

    can export a couple of csvs,
    still needs to be updated and run on a large dataset

--- MoviePageInfo.py
+++ MoviePageInfo.py
@@ -13,9 +13,10 @@ Description:
 
 
 
-
+import pickle
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 movieID = 0
 
@@ -24,136 +25,301 @@ imdb_url = 'https://www.imdb.com'
 #url = 'https://www.imdb.com/title/tt10366460/?ref_=adv_li_tt'
 
 #The Lost city movie url
-url = 'https://www.imdb.com/title/tt13320622/?ref_=adv_li_tt'
+#url = 'https://www.imdb.com/title/tt13320622/?ref_=adv_li_tt'
 
 #our flag means death tv show url
 #url = 'https://www.imdb.com/title/tt11000902/?ref_=adv_li_tt'
-
-
-soup = BeautifulSoup(requests.get(url).text,'lxml')
-
-# variables are named to match the names of the attributes of the relation schema
-# budget: there is no budget listing for movies
-movieID = url[27:37]
-title = soup.find('h1', class_ = 'sc-b73cd867-0 eKrKux').text #name
-
-temp = soup.find('div', attrs={'data-testid' : 'title-boxoffice-section'}) # grossing info
-grossingUS_CA = None
-grossingUS_CA_OpeningWeekend = None
-grossingWorldwide = None
-if(temp != None):
-    grossingUS_CA = temp.find('li', attrs={'data-testid' : 'title-boxoffice-grossdomestic'})
-    if(grossingUS_CA !=None):
-        grossingUS_CA = grossingUS_CA.find('li').text
-    grossingUS_CA_OpeningWeekend = temp.find('li', attrs={'data-testid' : 'title-boxoffice-openingweekenddomestic'})
-    if(grossingUS_CA_OpeningWeekend != None):
-        grossingUS_CA_OpeningWeekend = grossingUS_CA_OpeningWeekend.find('li').text
-    grossingWorldwide = temp.find('li', attrs={'data-testid' : 'title-boxoffice-cumulativeworldwidegross'})
-    if(grossingWorldwide != None):
-        grossingWorldwide = grossingWorldwide.find('li').text
-
-# had to use attrs={key : value} because the key had a hypen in it and python doesnt like that
-releaseDate = soup.find('li', attrs={'data-testid' : 'title-details-releasedate'}).a.find_next_sibling().text # release date
-countriesOfOrigin = soup.find('li', attrs={'data-testid' : 'title-details-origin'}).find_all('li') # countries of origin
-
-# temp variables initialized to make countires of origin
-temp2 = ""
-for x in countriesOfOrigin:
-    temp2 = temp2 + x.text + ", "
-countries_of_origin = temp2[:-2] # removes the last comma and space
-
-imdb_rating = soup.find('div', class_ = 'sc-7ab21ed2-2 kYEdvH').text.split('/')[0] # imdb rating
-popularity_score = soup.find('div', attrs={'data-testid' : 'hero-rating-bar__popularity__score'}).text #popularity score
-popularity_delta = soup.find('div', attrs={'data-testid' : 'hero-rating-bar__popularity__delta'}).text #popularity delta
-runtime = soup.find('li', attrs={'data-testid' : 'title-techspec_runtime'}).find('div').text #runtime
-color = soup.find('li', attrs={'data-testid' : 'title-techspec_color'}).find('li').text # color
-#aspectratio = soup.find('li', attrs= {'data-testid' : 'title-techspec_aspectratio'}).find('div').text # aspectratio
-
-
-print(movieID)
-print(title)
-print(grossingUS_CA)
-print(grossingUS_CA_OpeningWeekend)
-print(grossingWorldwide)
-print(releaseDate)
-print(countries_of_origin)
-print(imdb_rating)
-print(popularity_score)
-print(popularity_delta)
-print(runtime)
-print(color)
-#print(aspectratio)
-
-
-
-""" ------------------------------- cast --------------------------------------- """
-top_castlist = soup.find_all('a', attrs={'data-testid' : 'title-cast-item__actor'})
-
 castlinks = []
-for n in top_castlist:
-    castlinks.append(imdb_url + n.get('href'))
-#print(castlinks)
 
-"""
-for n in top_castlist:
-    cast_url = imdb_url + n.get('href')
-    #print(cast_url)
-    castname = n.text
-    castid = n.get('href')[6:15]
-    soup2 = BeautifulSoup(requests.get(cast_url).text,'lxml')
-    temp = soup2.find('div', attrs={'id':'name-born-info'})
+# test1.p holds movielinks data in a set of size 1162, and pulled links from comedy, scifi, horror and romance
+pickle_in = open('moviePagelinks_small.p', 'rb')
+movieLinks = pickle.load(pickle_in)
+pickle_in.close()
+
+#url = 'https://www.imdb.com/title/tt1570728/'
+#movieLinks = [url]
+
+global moviedf
+moviedf = pd.DataFrame(columns=['mID','name','releaseDate', 'popularity_score',
+                           'popularity_delta','color','runtime','aspectratio','imdb_rating',
+                           'grossingUS_CA','grossingUS_CA_OpeningWeekend','grossingWorldwide',
+                           ])
+def moviedf_row(mid, name, rd, ps, pd, col, runt, asp, rating, us_ca, us_ca_op, world):
+    row = {'mID':mid,'name':name,'releaseDate':rd,'popularity_score':ps,
+           'popularity_delta':pd,'color':col,'runtime':runt,'aspectratio':asp,'imdb_rating':rating,
+           'grossingUS_CA':us_ca,'grossingUS_CA_OpeningWeekend':us_ca_op,'grossingWorldwide':world}
+    return row
+
+global companyID
+companyID = 0
+movieproductioncompanydf = pd.DataFrame(columns=['companyID', 'mID', 'name'])
+def movieproductioncompanydf_row(companyID, mID, name):
+    row = {'companyID':companyID, 'mID':mID, 'name':name}
+    return row
+
+movieKeywordsdf = pd.DataFrame(columns=['keyword', 'mID'])
+def movieKeywordsdf_row(keyword,mID):
+    row = {'keyword':keyword, 'mID':mID}
+    return row
+
+movieGenresdf = pd.DataFrame(columns=['genre', 'mID'])
+def movieGenresdf_row(genre,mID):
+    row = {'genre':genre, 'mID':mID}
+    return row
+
+movieLanguagesdf = pd.DataFrame(columns=['language', 'mID'])
+def movieLanguagesdf_row(language,mID):
+    row = {'language':language, 'mID':mID}
+    return row
+
+global locationID
+locationID = 0
+locationsdf = pd.DataFrame(columns=['locationID', 'name', 'country'])
+def locationsdf_row(locationID, name, country):
+    row = {'locationID': locationID, 'name':name, 'country':country}
+    return row
+
+movieFilmedAtdf = pd.DataFrame(columns=['locationID', 'mID'])
+def movieFilmedAtdf_row(locationID, mID):
+    row = {'locationID':locationID, 'mID':mID}
+    return row
+
+for url in movieLinks:
+    
+    soup = BeautifulSoup(requests.get(url).text,'lxml')
+    print(url)
+    # variables are named to match the names of the attributes of the relation schema
+    # budget: there is no budget listing for movies
+    movieID = url[27:-1]
+    
+    #initialize every variable to None
+    title = None
+    grossingUS_CA = None
+    grossingUS_CA_OpeningWeekend = None
+    grossingWorldwide = None
+    releaseDate = None
+    countriesOfOrigin = None
+    imdb_rating = None
+    popularity_score = None
+    popularity_delta = None
+    runtime = None
+    color = None
+    aspectratio = None
+    productionCompanies = None
+    genres = None
+    keywords = None
+    languages = None
+    filmedAt = None
+    directors = None
+    writers = None
+    
+    
+    title = soup.find('h1', attrs={'data-testid' : 'hero-title-block__title'}).text #name
+    
+    temp = soup.find('div', attrs={'data-testid' : 'title-boxoffice-section'}) # grossing info
+
     if(temp != None):
-        dob = temp.time['datetime']
-        born_in = temp.find_all('a')[2].text
-    else:
-        dob = None
-        born_in = None
-    print(castid)
-    print(castname)
-    print(dob)
-    print(born_in)
-""" 
-""" ---------------------------------------------------------------------"""
+        grossingUS_CA = temp.find('li', attrs={'data-testid' : 'title-boxoffice-grossdomestic'})
+        if(grossingUS_CA !=None):
+            grossingUS_CA = grossingUS_CA.find('li').text
+        grossingUS_CA_OpeningWeekend = temp.find('li', attrs={'data-testid' : 'title-boxoffice-openingweekenddomestic'})
+        if(grossingUS_CA_OpeningWeekend != None):
+            grossingUS_CA_OpeningWeekend = grossingUS_CA_OpeningWeekend.find('li').text
+        grossingWorldwide = temp.find('li', attrs={'data-testid' : 'title-boxoffice-cumulativeworldwidegross'})
+        if(grossingWorldwide != None):
+            grossingWorldwide = grossingWorldwide.find('li').text
+    
+    # had to use attrs={key : value} because the key had a hypen in it and python doesnt like that
+    releaseDatesoup = soup.find('li', attrs={'data-testid' : 'title-details-releasedate'})
+    if(releaseDatesoup != None): releaseDate = releaseDatesoup.a.find_next_sibling().text # release dat
+    countriesOfOriginsoup = soup.find('li', attrs={'data-testid' : 'title-details-origin'})
+    if(countriesOfOriginsoup != None): 
+        countriesOfOrigin = countriesOfOriginsoup.find_all('li') # countries of origin
+        # temp variables initialized to make countires of origin
+        temp2 = ""
+        for x in countriesOfOrigin:
+            temp2 = temp2 + x.text + ", "
+            countries_of_origin = temp2[:-2] # removes the last comma and space
 
-prodsoup = soup.find('li', attrs={'data-testid' : 'title-details-companies'}).find('ul').find_all('li') # production companies
-productionCompanies = [x.text for x in prodsoup]
-print(productionCompanies)
+    ratingsoup = soup.find('div', attrs={'data-testid' : 'hero-rating-bar__aggregate-rating__score'})
+    if(ratingsoup != None): imdb_rating = ratingsoup.text.split('/')[0] # imdb rating
+    
+    popularitysoup_s = soup.find('div', attrs={'data-testid' : 'hero-rating-bar__popularity__score'})
+    if(popularitysoup_s != None): popularity_score = popularitysoup_s.text #popularity score
+    popularitysoup_d = soup.find('div', attrs={'data-testid' : 'hero-rating-bar__popularity__delta'})
+    if(popularitysoup_d != None): popularity_delta = popularitysoup_d.text #popularity delta
+    
+    runtimesoup = soup.find('li', attrs={'data-testid' : 'title-techspec_runtime'})
+    if(runtimesoup != None): runtime = runtimesoup.find('div').text #runtime
+    
+    colorsoup =soup.find('li', attrs={'data-testid' : 'title-techspec_color'})
+    if(colorsoup != None): color = colorsoup.find('li').text # color
+        
+    soundmixsoup = soup.find('li', attrs= {'data-testid' : 'title-techspec_soundmix'})
+    if(soundmixsoup != None): soundmix = soundmixsoup.find('div').text # aspectratio
+    
+    aspectratiosoup = soup.find('li', attrs= {'data-testid' : 'title-techspec_aspectratio'})
+    if(aspectratiosoup != None): aspectratio = aspectratiosoup.find('div').text # aspectratio
+    
+    """
+    print(movieID)
+    print(title)
+    print(grossingUS_CA)
+    print(grossingUS_CA_OpeningWeekend)
+    print(grossingWorldwide)
+    print(releaseDate)
+    print(countries_of_origin)
+    print(imdb_rating)
+    print(popularity_score)
+    print(popularity_delta)
+    print(runtime)
+    print(color)
+    print(aspectratio)
+    """
+    
+    
+    """ ------------------------------- cast --------------------------------------- """
+    top_castlist = soup.find_all('a', attrs={'data-testid' : 'title-cast-item__actor'})
+    
+    if(top_castlist != None):
+        for n in top_castlist:
+            castlinks.append((imdb_url + n.get('href'),movieID))
+    #print(castlinks)
+    
+    """
+    for n in top_castlist:
+        cast_url = imdb_url + n.get('href')
+        #print(cast_url)
+        castname = n.text
+        castid = n.get('href')[6:15]
+        soup2 = BeautifulSoup(requests.get(cast_url).text,'lxml')
+        temp = soup2.find('div', attrs={'id':'name-born-info'})
+        if(temp != None):
+            dob = temp.time['datetime']
+            born_in = temp.find_all('a')[2].text
+        else:
+            dob = None
+            born_in = None
+        print(castid)
+        print(castname)
+        print(dob)
+        print(born_in)
+    """ 
+    """ ---------------------------------------------------------------------"""
+    
+    prodsoup = soup.find('li', attrs={'data-testid' : 'title-details-companies'})
+    if(prodsoup != None):
+        prodlist = prodsoup.find('ul').find_all('li') # production companies
+        productionCompanies = [x.text for x in prodlist]
+    
+    genresoup = soup.find('li', attrs={'data-testid' : 'storyline-genres'})
+    if(genresoup != None):
+        genrelist = genresoup.find_all('li') # genres
+        genres = [x.text for x in genrelist] 
+    
+    # it only takes the 5 keywords displayed on the page, getting the rest will require navigating to another link, which
+    # takes a little while computationally
+    keywordsoup = soup.find('div', attrs={'data-testid' : 'storyline-plot-keywords'})
+    if(keywordsoup != None):
+        keywordlist = keywordsoup.find_all('a') # keywords
+        keywords = [x.text for x in keywordlist[:-1]] # if you take keywordsoup you get the '5 more keywords...' item, so i took out the last item
+    
+    langsoup = soup.find('li', attrs={'data-testid' : 'title-details-languages'})
+    if(langsoup != None):
+        langslist = langsoup.find('ul').find_all('li')
+        languages = [x.text for x in langslist]
+    
+    filmedatsoup = soup.find('li', attrs={'data-testid' : 'title-details-filminglocations'})
+    if(filmedatsoup != None):
+        filmedatlocations = filmedatsoup.find('ul').find_all('li')
+        filmedAt = [x.text for x in filmedatlocations]
+    
+    importantpeoplesoup = soup.find('div', attrs={'data-testid' : 'title-pc-wide-screen'})
+    if(importantpeoplesoup!= None):
+        importantpeoplesoup = importantpeoplesoup.find_all('li', attrs={'data-testid': 'title-pc-principal-credit'})
+        for x in importantpeoplesoup:
+            
+            if(x.span != None):
+                if(x.span.text == 'Directors' or x.span.text == 'Director'):
+                    directorsoup = x.find_all('li', class_ = 'ipc-inline-list__item')
+                    if(directorsoup != None):
+                        directors = [x.a.text for x in directorsoup]
+                if(x.span.text == 'Writers' or x.span.text == 'Writer'):
+                    writersoup = x.find_all('li', class_ = 'ipc-inline-list__item')
+                    if(writersoup != None):
+                        writers = [x.a.text for x in writersoup]
+            if(x.a != None):
+                if(x.a.text == 'Writers' or x.a.text == 'Writer'):
+                    writersoup = x.find_all('li', class_ = 'ipc-inline-list__item')
+                    if(writersoup != None):
+                        writers = [x.a.text for x in writersoup]
 
-genresoup = soup.find('li', attrs={'data-testid' : 'storyline-genres'}).find_all('li') # genres
-genres = [x.text for x in genresoup]
-print(genres)
+    """
+    print(productionCompanies)
+    print(genres)
+    print(keywords)
+    print(languages)
+    print(filmedAt)
+    print(directors)
+    print(writers)
+    print()
+    """
 
-# it only takes the 5 keywords displayed on the page, getting the rest will require navigating to another link, which
-# takes a little while computationally
-keywordsoup = soup.find('div', attrs={'data-testid' : 'storyline-plot-keywords'}).find_all('a') # keywords
-keywords = [x.text for x in keywordsoup[:-1]] # if you take keywordsoup you get the '5 more keywords...' item, so i took out the last item
-print(keywords)
+    
+    
+    """ ---------------- adding data into the dataframe then exporting that dataframe as a csv --------------"""
+    
+    
+    
+    moviedf = moviedf.append(
+                    moviedf_row(movieID, title, releaseDate,
+                    popularity_score,popularity_delta,color,runtime,
+                    aspectratio, imdb_rating, grossingUS_CA,
+                    grossingUS_CA_OpeningWeekend, grossingWorldwide), ignore_index=True
+                    )
+    
+    if(productionCompanies!=None):
+        for i in productionCompanies:
+            #https://stackoverflow.com/questions/30944577/check-if-string-is-in-a-pandas-dataframe
+            if(~movieproductioncompanydf['name'].str.contains(i).any()):
+                companyID = companyID + 1
+                movieproductioncompanydf = movieproductioncompanydf.append(
+                        movieproductioncompanydf_row(companyID,movieID, i), ignore_index = True)
+    if(keywords!=None):
+        for i in keywords:
+            movieKeywordsdf = movieKeywordsdf.append(
+                        movieKeywordsdf_row(i,movieID), ignore_index = True)
+    
+    if(genres!=None):
+        for i in genres:
+            movieGenresdf = movieGenresdf.append(
+                        movieGenresdf_row(i,movieID), ignore_index = True)
+       
+    if(languages!=None):
+        for i in languages:
+            movieLanguagesdf = movieLanguagesdf.append(
+                movieLanguagesdf_row(i,movieID), ignore_index = True)
+    
+    if(filmedAt!=None):
+        for i in filmedAt:
+            country = i.split(',')[-1].strip()
+            if(country!=i):
+                name = i.split(',')[-2].strip()
+            else:
+                name = i
+            
+            if(~locationsdf['name'].str.contains(name).any()):
+                locationID = locationID + 1
+                locationsdf = locationsdf.append(
+                        locationsdf_row(locationID,name,country), ignore_index = True)
+            
+            
+    
+"end for"
 
-langsoup = soup.find('li', attrs={'data-testid' : 'title-details-languages'}).find('ul').find_all('li')
-languages = [x.text for x in langsoup]
-print(languages)
-
-filmedatlocations = soup.find('li', attrs={'data-testid' : 'title-details-filminglocations'}).find('ul').find_all('li')
-filmedAt = [x.text for x in filmedatlocations]
-print(filmedAt)
-
-importantpeople = soup.find('div', class_ = 'sc-fa02f843-0 fjLeDR')
-directorsoup = importantpeople.find('li').find_all('li', class_ = 'ipc-inline-list__item')
-writersoup = importantpeople.find('li').find_next_sibling().find_all('li', class_ = 'ipc-inline-list__item')
-
-directors = [x.text for x in directorsoup]
-writers = [x.a.text for x in writersoup]
-print(directors)
-print(writers)
-
-
-#print(releaseDate)
-#grossing = soup.find_all('span', class_ = 'ipc-metadata-list-item__list-content-item')
-#for x in grossing:
-#    print(x.text)
-
-#for tag in releaseDate.find_all(True):
-#    print(tag.name)
-
-
-#print(grossing)
+moviedf.to_csv(r'movies.csv', index=False)
+movieproductioncompanydf.to_csv(r'movieProductionCompanies.csv', index=False)
+movieKeywordsdf.to_csv(r'movieKeywords.csv', index=False)
+movieGenresdf.to_csv(r'movieGenres.csv', index=False)
+movieLanguagesdf.to_csv(r'movieLanguages.csv', index=False)
+locationsdf.to_csv(r'locations.csv', index = False)
