9f67f1b26ee03f0a185c6424cd9cb1e4ffbfc9ed
LRotenberger <lrotenberger@ufl.edu>
2022-04-13 16:25:46 -0400

    chart can now be dynamically updated by user input for query

--- filmanalyzer/frontend/src/pages/Test.jsx
+++ filmanalyzer/frontend/src/pages/Test.jsx
@@ -5,39 +5,43 @@ import { executeQuery } from "../utils/api";
 import TextField from "@mui/material/TextField";
 
 export default function Test() {
-  async function doQuery() {
-    let res = await executeQuery('SELECT * FROM "JONATHAN.CUNNING".MOVIE');
-    console.log(res);
+  const [inputText, setInputText] = React.useState("");
+  const [data, setData] = React.useState([
+    { label: "React Charts", data: [{ x: 0, y: 0 }] },
+  ]);
+  const [yaxis, setYaxis] = React.useState("Y-Axis");
+  const [xaxis, setXaxis] = React.useState("X-Axis");
+
+  async function searchCast(name) {
+    let res = await executeQuery(
+      'SELECT * FROM "JONATHAN.CUNNING".CAST WHERE lower (CASTNAME)=\'' +
+        name +
+        "'"
+    );
+    return res;
+  }
+
+  async function setRoles() {
+    let temp = await searchCast(inputText);
+    temp = temp.rows;
+    console.log(temp);
+    setData([
+      {
+        label: "Roles",
+        data: temp.map(function (row) {
+          return { x: row.BIRTH, y: row.CASTID };
+        }),
+      },
+    ]);
+    setYaxis("Cast ID");
+    setXaxis("Birth Year");
   }
 
   const navigate = useNavigate();
-  const data = [
-    {
-      label: "React Charts",
-
-      data: [
-        {
-          primary: 1,
-
-          likes: 130,
-        },
-
-        {
-          primary: 2,
-
-          likes: 150,
-        },
-        {
-          primary: 3,
-          likes: 50,
-        },
-      ],
-    },
-  ];
 
   const primaryAxis = React.useMemo(
     () => ({
-      getValue: (datum) => datum.primary,
+      getValue: (datum) => datum.x,
     }),
 
     []
@@ -45,12 +49,17 @@ export default function Test() {
   const secondaryAxes = React.useMemo(
     () => [
       {
-        getValue: (datum) => datum.likes,
+        getValue: (datum) => datum.y,
       },
     ],
 
     []
   );
+
+  let inputLog = (e) => {
+    var lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
   return (
     <div className="flex-col flex space-y-5 h-full">
       <h1 className="text-3xl font-bold underline text-center">
@@ -59,12 +68,13 @@ export default function Test() {
       <div className="flex flex-row justify-center">
         <TextField
           id="outlined-basic"
+          onChange={inputLog}
           variant="outlined"
           label="Search for Actors/Cast"
         />
         <button
           className="flex bg-slate-300 rounded-md px-3 py-3"
-          onClick={() => navigate("/")}
+          onClick={() => searchCast(inputText)}
         >
           Go
         </button>
@@ -78,7 +88,7 @@ export default function Test() {
         </button>
         <button
           className="flex bg-slate-300 rounded-md px-3 py-2"
-          onClick={() => doQuery()}
+          onClick={() => setRoles()}
         >
           Roles
         </button>
@@ -92,15 +102,13 @@ export default function Test() {
       <div className="flex flex-1 flex-col">
         <div className="flex flex-1">
           <h1 className="flex flex-col justify-center max-w-[5rem] break-all">
-            Y-Axis
+            {yaxis}
           </h1>
           <div className="flex-1 relative">
             <Chart
               options={{
                 data,
-
                 primaryAxis,
-
                 secondaryAxes,
               }}
             />
@@ -108,7 +116,7 @@ export default function Test() {
         </div>
         <div className="flex justify-between">
           <br></br>
-          <h1>X-Axis</h1>
+          <h1>{xaxis}</h1>
           <div className="flex bg-slate-300 rounded-md px-3 py-2">
             <button onClick={() => navigate("/")}>Back</button>
           </div>
